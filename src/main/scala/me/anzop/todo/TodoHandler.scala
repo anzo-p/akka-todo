e3d39b7721743e2112f1bcfb3ba79472bc599b22
Anssi Pitkänen <anssi.pitkanen@wolt.com>
2022-09-09 09:12:08 +0300

    rolling timeout on actor passivation

--- src/main/scala/me/anzop/todo/TodoHandler.scala
+++ src/main/scala/me/anzop/todo/TodoHandler.scala
@@ -1,6 +1,6 @@
 package me.anzop.todo
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Cancellable, Props}
 
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
@@ -9,24 +9,33 @@ trait TodoHandler {
 
   val actorLifetime: FiniteDuration       = 10 seconds
   var activeActors: Map[String, ActorRef] = Map()
+  var timeouts: Map[String, Cancellable]  = Map()
+
+  def scheduleTimeout(name: String, actor: ActorRef): Cancellable =
+    system.scheduler.scheduleOnce(actorLifetime, passivate(name, actor))(system.dispatcher)
+
+  def passivate(name: String, actor: ActorRef): Runnable = () => {
+    actor ! TodoHandlerActor.Shutdown
+    activeActors -= name
+    timeouts     -= name
+    system.log.info(s"passivating actor $name, state now: $activeActors")
+  }
 
   def todoHandler(userId: String): ActorRef = {
     val wouldBeActor = s"todo-handler-$userId"
 
     if (activeActors.contains(wouldBeActor)) {
       system.log.info(s"discovered existing actor ${activeActors(wouldBeActor)}")
-      activeActors(wouldBeActor)
+      val actor = activeActors(wouldBeActor)
+      timeouts(wouldBeActor).cancel()
+      timeouts += wouldBeActor -> scheduleTimeout(wouldBeActor, actor)
+      actor
+
     } else {
       val actor = system.actorOf(Props(new TodoHandlerActor(userId)), wouldBeActor)
       system.log.info(s"created new actor $actor")
-      activeActors += (wouldBeActor -> actor)
-
-      val passivate: Runnable = () => {
-        actor ! TodoHandlerActor.Shutdown
-        activeActors -= wouldBeActor
-      }
-      system.scheduler.scheduleOnce(actorLifetime, passivate)(system.dispatcher)
-
+      activeActors += wouldBeActor -> actor
+      timeouts     += wouldBeActor -> scheduleTimeout(wouldBeActor, actor)
       actor
     }
   }
